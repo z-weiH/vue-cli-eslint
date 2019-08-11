16c52b16b8daace7259b4839abae3738f06a5605
zengwei <18279047775@163.com>
2019-08-11 17:18:15 +0800

    增加 eslint配置项以及相关文档

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,11 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-trailing-spaces": 1,//一行结束后面不要有空格
+    "no-extra-semi": 2,//禁止多余的冒号
   },
   parserOptions: {
     parser: 'babel-eslint'
-  }
+  },
 }
